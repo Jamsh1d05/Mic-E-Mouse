7e4fc079ec1c75ba547ad9df0048888d4417eb40
Jamshid <jamshidalimzhanov@gmail.com>
2025-11-11 15:25:43 +0500

    fix:import error fixed

--- audio_postanalys.py
+++ audio_postanalys.py
@@ -1,4 +1,5 @@
 import librosa.display, matplotlib.pyplot as plt
-y, sr = librosa.load("enhanced.wav")
+import numpy as np
+y, sr = librosa.load("mouse_sound.wav")
 librosa.display.specshow(librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max), sr=sr)
 plt.show()
